33958e932b36b5b755b16e77f3c9b29d45c18c30
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-06-16 16:39:36 +0530

    add a middleware and a function to handle errors

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,19 +1,21 @@
 import User from '../model/user.model.js'
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
-        if (!username || !email || !password) {
-            return res.status(404).json({ message: "Pls fill all fields are required" })
+        if (!username || !email || !password || username == '' || email == '' || password == '') {
+            // return res.status(404).json({ message: "Pls fill all fields are required" })
+            next(errorHandler(400, 'All fields are required!'))
         }
         const hashedPassword = bcryptjs.hashSync(password, 10)
         const newUser = new User({ username, email, password: hashedPassword });
         const data = await newUser.save();
         res.status(200).json({ message: "Signup Successful", data: data });
     }
-    catch (err) {
-        console.log("Error", err);
-        res.status(402).json({ message: "Server Error" });
+    catch (error) {
+        console.log("Error", error);
+        next(error)
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,21 @@ const app = express();
 app.use(express.json())
 connectDB()
 
+
 app.use('/api/user', userRoute);
 app.use('/auth/user', authRoute);
 // express json is middlleware which is used for pass the JSON data form frontend to backend ||
 
+// MiddleWare 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
